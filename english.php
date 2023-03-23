99d624a7c3b73726942e8bc0907bc22f2738f22f
MD Mahfuz Reham <78415545+mahfuzreham@users.noreply.github.com>
2023-03-23 23:21:20 +0600

    Add files via upload

---
+++ english.php
@@ -0,0 +1,11 @@
+<?php
+$_ADDONLANG['nopin'] = "You didnt have generated a PIN";
+$_ADDONLANG['havepin'] = "Your PIN is: ";
+$_ADDONLANG['message'] = "To verificate the ownership of your accout please provide this PIN to our support. Your PIN is 24 hours valid.";
+$_ADDONLANG['generatebutton'] = "Generate PIN";
+
+// for sidebares at whmcs v6 / v7
+$_ADDONLANG['sidebar_nopin'] = "No PIN Code generated.";
+$_ADDONLANG['sidebar_havepin'] = "Your current PIN is: ";
+$_ADDONLANG['sidebar_generatebutton'] = "Generate new PIN";
+$_ADDONLANG['sidebar_title'] = "Support PIN";
---
+++ german.php
@@ -0,0 +1,11 @@
+<?php
+$_ADDONLANG['nopin'] = "Sie haben noch keinen PIN erstellt";
+$_ADDONLANG['havepin'] = "Ihr aktueller PIN lautet: ";
+$_ADDONLANG['message'] = "Um Ihre Identit&auml;t im Livechat zu best&auml;tigen, k&ouml;nnen Sie hier einen PIN-Code generieren. Der PIN ist aus Sicherheitsgr&uuml;nden nur 24 Stunden g&uuml;ltig.";
+$_ADDONLANG['generatebutton'] = "PIN Generieren";
+
+// for sidebares at whmcs v6 / v7
+$_ADDONLANG['sidebar_nopin'] = "Noch kein PIN erstellt.";
+$_ADDONLANG['sidebar_havepin'] = "Ihr aktueller PIN lautet: ";
+$_ADDONLANG['sidebar_generatebutton'] = "PIN Generieren";
+$_ADDONLANG['sidebar_title'] = "Support PIN";
